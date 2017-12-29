7707f2ecfab8a9a9eb4d8e13a7dd9c593c569ccf
Jason Kwong <jasonkwong11@gmail.com>
2017-12-28 20:29:45 -0500

    update syntax errors in derivingInstances.hs

--- derivingOrdInstances.hs
+++ derivingOrdInstances.hs
@@ -1,3 +1,11 @@
-data DayOfWeek
+data DayOfWeek =
   Mon | Tue | Weds | Thu | Fri | Sat | Sun
-  deriving (Ord, Show, Eq)
+  deriving (Show, Eq)
+-- notice how Ord is removed from deriving (Ord, Show, Eq)
+
+instance Ord DayOfWeek where
+  compare Fri Fri   = EQ
+  compare Fri _     = GT
+  compare _ Fri     = LT
+  compare _ _       = EQ
+
