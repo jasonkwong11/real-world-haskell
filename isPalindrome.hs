f6b12c97c62bcff0f7ca542b008826509e604292
Jason Kwong <jasonkwong11@gmail.com>
2018-02-05 17:12:48 -0500

    update

--- isPalindrome.hs
+++ isPalindrome.hs
@@ -5,6 +5,8 @@ isPalindrome x = bool
   where reversedString = reverse x
         bool = (reversedString == x)
 
+
+
 myAbs :: Integer -> Integer
 myAbs num = absVal
   where absVal = if num < 0 then num * (-1) else num
@@ -12,3 +14,4 @@ myAbs num = absVal
 
 plusOne string = lengthPlusOne
   where lengthPlusOne = length string + 1
+
